820120300b24965f0fa13723a66322990903eaa1
Kira <kirnan_t@outlok.com>
2025-05-06 13:08:19 +0700

    add albums

---
+++ tasc1/musicCollection.js
@@ -0,0 +1,12 @@
+const albums = [
+    { title: "Kill 'Em All", artist: "Metallica", year: "1983" },
+    { title: "Ride the Lightning", artist: "Metallica", year: "1984" },
+    { title: "Master of Puppets", artist: "Metallica", year: "1986" },
+    { title: "...And Justice for All", artist: "Metallica", year: "1988" },
+    { title: "Metallica (The Black Album)", artist: "Metallica", year: "1991" },
+    { title: "Load", artist: "Metallica", year: "1996" },
+    { title: "Reload", artist: "Metallica", year: "1997" },
+    { title: "St. Anger", artist: "Metallica", year: "2003" },
+    { title: "Death Magnetic", artist: "Metallica", year: "2008" },
+    { title: "Hardwired... to Self-Destruct", artist: "Metallica", year: "2016" }
+];
